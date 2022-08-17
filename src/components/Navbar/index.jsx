74011dc99e281c746e26a69e1d9aa2c4a0bd7cfa
GuilleContreras <guille.contre1@gmail.com>
2022-08-16 22:39:32 -0300

    Agregado NavBar simple

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,11 @@
+const Navbar = () =>{
+    return (
+    <nav>
+        <p>Somos Guitar Budgets</p>
+        <a href="">Inicio</a>
+        <a href="">Contacto</a>
+    </nav>
+    )
+}
+
+export default Navbar
