3ffdb70c4d935b0ebb7023cb109f434bececb900
GuilleContreras <guille.contre1@gmail.com>
2022-09-26 19:44:16 -0300

    Entrega Desafio Cart View

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -7,6 +7,7 @@ import { useContext } from 'react';
 import { Button } from 'react-bootstrap';
 import '../Cart/cartItem.css';
 import { CartContext } from '../../context/CartContext';
+import { Link } from 'react-router-dom';
 
 const articlesImg = require.context(`../assets/articles`, true)
 
@@ -17,48 +18,64 @@ const CartItem = () => {
     const removeItemButtonHandler = (item) =>{
       removeItem(item);
     }
-  
+console.log(cart)
+console.log(cart.length)
 
-  return (
-    <div> 
-    {cart.map(producto => {
+    if(cart.length === 0){
+
         return (
-            <Container fluid className='p-2' key={producto.item.id}>
-        <Card className='cartCard'>
-            <Row>
-                <Col >
-                    <Card.Title>Producto</Card.Title>
-                    <Card.Title className="text-center mt-5 mb-4">{producto.item.name}</Card.Title>
-                </Col>
-                <Col>
-                    <Card.Title>Description</Card.Title>
-                    <Card.Text className="text-center mt-4 mb-4">
-                    {producto.item.description}
-                    </Card.Text>
-                </Col>
-                <Col>
-                    <Card.Title>Image</Card.Title>
-                    <img className="img-fluid detailImg" alt="imgDetail" variant="top" src={articlesImg(`./${producto.item.image}`)} />
-                </Col>
-                <Col>
-                    <Card.Title>Cantidad</Card.Title>
-                    <Card.Text className="text-center mt-4 mb-4">
-                    {producto.quantity}
-                    </Card.Text>
-                </Col>
-                <Col>
-                    <Card.Title>Erase item</Card.Title>
-                    <Button variant="light" className="text-center mt-4 mb-4" onClick={() => removeItemButtonHandler(producto.item.id)}><img src="boton-x.png" alt="" /></Button>
-                </Col>
-            </Row>
-            
-        </Card>
-    </Container>
+            <>
+            <h1 className='cartItemH1'>No hay poductos agregados al carrito</h1>
+            <h2 className='cartItemH2'>Para agregar items vuelve a nuestra página de productos</h2>
+            <Link to={`/productos`}><Button variant="primary" className="btnText m-2">Ir a Home</Button></Link>
+            </>
         )
-    })
+    }else{
+
+        return (
+            <div> 
+            {cart.map(producto => {
+                return (
+                    <Container fluid className='p-2' key={producto.item.id}>
+                <Card className='cartCard'>
+                    <Row>
+                        <Col >
+                            <Card.Title>Producto</Card.Title>
+                            <Card.Title className="text-center mt-5 mb-4">{producto.item.name}</Card.Title>
+                        </Col>
+                        <Col>
+                            <Card.Title>Description</Card.Title>
+                            <Card.Text className="text-center mt-4 mb-4">
+                            {producto.item.description}
+                            </Card.Text>
+                        </Col>
+                        <Col>
+                            <Card.Title>Image</Card.Title>
+                            <img className="img-fluid detailImg" alt="imgDetail" variant="top" src={articlesImg(`./${producto.item.image}`)} />
+                        </Col>
+                        <Col>
+                            <Card.Title>Cantidad</Card.Title>
+                            <Card.Text className="text-center mt-4 mb-4">
+                            {producto.quantity}
+                            </Card.Text>
+                        </Col>
+                        <Col>
+                            <Card.Title>Erase item</Card.Title>
+                            <Button variant="light" className="text-center mt-4 mb-4" onClick={() => removeItemButtonHandler(producto.item.id)}><img src="boton-x.png" alt="" /></Button>
+                        </Col>
+                    </Row>
+                    
+                </Card>
+            </Container>
+                )
+            })
+            }
+            </div>
+          );    
+        }
+
     }
-    </div>
-  );    
-}
+
+  
 
 export default CartItem;
